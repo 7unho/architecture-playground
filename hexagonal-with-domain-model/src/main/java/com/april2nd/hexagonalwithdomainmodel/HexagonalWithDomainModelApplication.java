75bcd6b22ac052f3de3cd4bd8d19adf4ffcfb2a4
7unho <rlawnsgh8395@naver.com>
2025-08-04 22:35:13 +0900

    :sparkles: Feat: add hexagonal architecture module

---
+++ hexagonal-with-domain-model/src/main/java/com/april2nd/hexagonalwithdomainmodel/HexagonalWithDomainModelApplication.java
@@ -0,0 +1,13 @@
+package com.april2nd.hexagonalwithdomainmodel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HexagonalWithDomainModelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HexagonalWithDomainModelApplication.class, args);
+	}
+
+}
---
+++ hexagonal-with-domain-model/src/test/java/com/april2nd/hexagonalwithdomainmodel/HexagonalWithDomainModelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.april2nd.hexagonalwithdomainmodel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HexagonalWithDomainModelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
